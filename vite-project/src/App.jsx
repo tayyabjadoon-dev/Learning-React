65c283e08871e7f1ce17fdc60bcdd2b4667e654d
Tayyab Jadoon <tayyabjadoon.dev@gmail.com>
2026-07-21 01:40:42 -0700

    Add initial React setup with Vite and create basic components

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import Youtube from './youtube.jsx'
+
+function App() {
+  return (
+    <div>
+    <h1>Vite React App</h1>
+    <Youtube />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ vite-project/src/youtube.jsx
@@ -0,0 +1,6 @@
+function Youtube() {
+  return (
+      <h1>Youtube</h1>
+  )
+}
+export default Youtube
